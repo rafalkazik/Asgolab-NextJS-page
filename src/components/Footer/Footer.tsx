404145f7491a7a760b605742fa2680c35bf29d71
Rafał Kazik <kazik.raf@gmail.com>
2022-05-12 22:54:35 +0200

    create footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,27 @@
 const Footer = () => {
   return (
     <footer className='footer'>
-      <div>FOOTER</div>
+      <div className='footer-container'>
+        <div className='footer__content'>
+          <p className='footer__content-text'>asgolab © 2022</p>
+          <p className='footer__content-text'>laboratorium protetyczne</p>
+        </div>
+        <div className='footer__content'>
+          <a
+            href='https://www.facebook.com/asgolab.protetyka'
+            className='footer__content-text'
+          >
+            facebook.com/asgolab.protetyka
+          </a>
+        </div>
+        <div className='footer__content footer__content--logo'>
+          <img
+            src='/ASGOLAB_logo_footer.svg'
+            alt='ASGOLAB logo'
+            className='footer__logo'
+          />
+        </div>
+      </div>
     </footer>
   );
 };
