3e703dbea4febc36e06c1715b52044c2e2a56062
Rafał Kazik <kazik.raf@gmail.com>
2022-05-12 21:20:41 +0200

    basic hero

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,7 +1,28 @@
+import { Link } from 'react-scroll';
+
 const Hero = () => {
   return (
     <div className='hero' id='hero'>
-      <div>HERO</div>
+      <div className='hero-container'>
+        <div className='hero__content'>
+          <h1 className='hero__content-text'>
+            Laboratorium protetyczne specjalizujące się w usługach zakresu
+            nowoczesnej techniki dentystyczne CAD/CAM, znajdujące się w centrum
+            Tychów.{' '}
+          </h1>
+          <Link
+            to='offer'
+            spy={true}
+            smooth={true}
+            offset={-112}
+            duration={500}
+          >
+            <button className='hero__content-button'>
+              Poznaj zakres usług
+            </button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import { Link } from 'react-scroll';
 
 const NavBar = () => {
