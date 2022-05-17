b5ca3c5ad2e3efdca97a478ce98ecdeb0d537437
Rafał Kazik <kazik.raf@gmail.com>
2022-05-17 19:39:07 +0200

    fixes, animated background lottie

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,9 +1,28 @@
+import { useEffect, useRef } from 'react';
 import { Link } from 'react-scroll';
+import Lottie from 'lottie-web';
 
 const Hero = () => {
+  const backgroundDesktop = useRef<any>(null);
+
+  useEffect(() => {
+    const instance = Lottie.loadAnimation({
+      container: backgroundDesktop.current,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      path: 'HeroLinesDesktop.json',
+    });
+    return () => instance.destroy();
+  });
+
   return (
     <div className='hero' id='hero'>
       <div className='hero-container'>
+        <div
+          className='hero__content-background-desktop'
+          ref={backgroundDesktop}
+        />
         <div className='hero__content'>
           <h1 className='hero__content-text'>
             Laboratorium protetyczne specjalizujące się w usługach zakresu
